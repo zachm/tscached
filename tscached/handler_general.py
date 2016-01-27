4c09d0ba60a66521152fd3c7d58b6f80e130c71a
Zach Musgrave <ztm@zachm.us>
2016-01-27 17:05:44 -0500

    supports a straight proxy

--- tscached/handler_general.py
+++ tscached/handler_general.py
@@ -3,10 +3,32 @@ import simplejson as json
 import yaml
 
 from flask import make_response
+from flask import request
+import requests
 
 from tscached import app
 
+KAIROS_HOST = 'localhost'
+KAIROS_PORT = 8080
 
 @app.route('/', methods=['GET'])
 def handle_root():
     return "hello world!"
+
+
+def query_kairos(query):
+    """ do it. """
+    url = 'http://%s:%s/api/v1/datapoints/query' % (KAIROS_HOST, KAIROS_PORT)
+    r = requests.post(url, data=json.dumps(query))
+    return json.loads(r.text)
+
+
+
+@app.route('/api/v1/datapoints/query', methods=['POST', 'GET'])
+def handle_query():
+    if request.method == 'POST':
+        query = json.loads(request.data)
+    else:
+        query = request.args.get('query')
+
+    return json.dumps(query_kairos(query))
