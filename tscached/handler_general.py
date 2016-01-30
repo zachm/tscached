86b153559ac6861d9e8c8c10349e0b3f67e0049b
Zach Musgrave <ztm@zachm.us>
2016-01-29 22:38:48 -0500

    WIP on how to hash. MTS level, or query level?

--- tscached/handler_general.py
+++ tscached/handler_general.py
@@ -45,6 +45,54 @@ def create_key(data, tipo):
     return key
 
 
+def series_equivalent(a, b):
+    """ Given dicts from .queries[].results[].* - verify if they match one another.
+        Much of this is O(n^2) - we presume n will be rather small.
+    """
+    # metric names must be the same
+    if a['name'] != b['name']:
+        return False
+
+    # same number of tags
+    if len(a['tags']) != len(b['tags']):
+        return False
+    for tag_key in a['tags']:
+        if a[tag_key] != b[tag_key]:
+            return False
+
+    # same number of groupings
+    if len(a['group_by']) != len(b['group_by']):
+        return False
+    for grouping in a['group_by']:
+        if grouping not in b['group_by']:
+            return False
+    return True
+
+
+def create_timeseries_key(result):
+    """ Given a result (single TS dict), return a hash describing its semantics. """
+
+    big_concat = result['name'] + '::'
+
+    for tag in sorted(result['tags'].keys()):
+        big_concat += tag + ':' + sorted(result['tags'][tag]).join(',')
+    big_concat += '::'
+
+    groupings = []
+    for grouping in result['group_by']:
+        grouping_str = ''
+        for key in sorted(grouping.keys()):
+            grouping_str += '%s.%s' % (key, grouping[key])
+        groupings.append(grouping_str)
+    big_concat += '::' + sorted(groupings).join(',')
+
+    return 'tscached::mts::' + hashlib.sha224(big_concat).hexdigest()
+
+
+
+
+
+
 @app.route('/api/v1/datapoints/query', methods=['POST', 'GET'])
 def handle_query():
     if request.method == 'POST':
