bd12492628b0a28c34c3bd630c5c7f3a7db8e3d1
Zach Musgrave <ztm@zachm.us>
2016-02-10 17:40:10 -0800

    tox rocks

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Always prefer setuptools over distutils
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='tscached',
+    version='0.0.1',
+
+    description='Caching proxy for time series data',
+    long_description='Advanced caching proxy for KairosDB, using Redis as a datastore.',
+
+    url='https://github.com/zachm/tscached',
+
+    author='Zach Musgrave',
+    author_email='ztm@zachm.us',
+    license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Framework :: Flask',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='metrics proxy caching kairosdb redis',
+
+    packages=find_packages(exclude=['tests', 'testing']),
+
+    install_requires=[],
+
+    extras_requires={
+        'testing': ['mock'],
+    },
+)
