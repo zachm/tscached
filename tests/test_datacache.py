a9ac73c7c9480db99014ab3b49959354720a8e85
Zach Musgrave <ztm@zachm.us>
2016-02-22 20:03:09 -0800

    split up test_datacache into sane cases

--- tests/test_datacache.py
+++ tests/test_datacache.py
@@ -5,29 +5,40 @@ from tscached.datacache import DataCache
 
 
 @patch('tscached.datacache.create_key', autospec=True)
-def datacache(m_create_key):
+def test__init__(m_create_key):
     m_create_key.return_value = 'some-redis-key'
-
     redis_cli = MockRedis()
 
     dc = DataCache(redis_cli, 'sometype')
-    dc.expiry = 9001
 
-    # test __init__
     assert dc.redis_client == redis_cli
     assert dc.cache_type == 'sometype'
 
-    # test make_key
+
+@patch('tscached.datacache.create_key', autospec=True)
+def test_make_key(m_create_key):
+    m_create_key.return_value = 'some-redis-key'
+    dc = DataCache(MockRedis(), 'sometype')
     dc.make_key()
     m_create_key.assert_called_once_with('{}', 'sometype')
     assert dc.redis_key == 'some-redis-key'
 
-    # test get_key
+
+@patch('tscached.datacache.create_key', autospec=True)
+def test_get_key(m_create_key):
+    m_create_key.return_value = 'some-redis-key'
+    dc = DataCache(MockRedis(), 'sometype')
     dc.redis_key = None
     assert dc.get_key() == 'some-redis-key'
-    assert m_create_key.call_count == 2
+    assert m_create_key.call_count == 1
 
-    # test set_cached
+
+@patch('tscached.datacache.create_key', autospec=True)
+def test_set_cached(m_create_key):
+    m_create_key.return_value = 'some-redis-key'
+    redis_cli = MockRedis()
+    dc = DataCache(redis_cli, 'sometype')
+    dc.expiry = 9001
     dc.set_cached('value-to-set')
     assert redis_cli.set_call_count == 1
     assert redis_cli.set_parms == [['some-redis-key', '"value-to-set"', {'ex': 9001}]]
@@ -37,12 +48,21 @@ def datacache(m_create_key):
     assert redis_cli.set_call_count == 2
     assert redis_cli.set_parms[1] == ['some-redis-key', '"some-other-value"', {'ex': 9001}]
 
-    # test process_cached_data
+
+@patch('tscached.datacache.create_key', autospec=True)
+def test_process_cached_data(m_create_key):
+    m_create_key.return_value = 'some-redis-key'
+    dc = DataCache(MockRedis(), 'sometype')
     assert dc.process_cached_data(None) is False
     assert dc.process_cached_data(False) is False
     assert dc.process_cached_data('"value-I-got"') == 'value-I-got'
 
-    # test get_cached
+
+@patch('tscached.datacache.create_key', autospec=True)
+def test_get_cached(m_create_key):
+    m_create_key.return_value = 'some-redis-key'
+    redis_cli = MockRedis()
+    dc = DataCache(redis_cli, 'sometype')
     assert dc.get_cached() == {'hello': 'goodbye'}
     assert redis_cli.get_call_count == 1
     assert redis_cli.get_parms == [['some-redis-key']]
